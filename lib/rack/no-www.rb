371ea7d4d58ab25f7c45ebdbe56cd60cce1d240a
Sean Coleman <sean@seancoleman.net>
2014-02-23 16:37:28 -0700

    Adds capability for specifying exclusive hosts to perform redirect on

--- lib/rack/no-www.rb
+++ lib/rack/no-www.rb
@@ -5,19 +5,33 @@ module Rack
 
     STARTS_WITH_WWW = /^www\./i
 
-    def initialize(app)
+    def initialize(app, options={})
+      default_options = {
+        only_hosts: []
+      }
+
       @app = app
+      @options = default_options.merge(options)
     end
 
     def call(env)
-      if env['HTTP_HOST'] =~ STARTS_WITH_WWW
+      if require_direct?
         [301, no_www_request(env), ["Moved Permanently\n"]]
       else
         @app.call(env)
       end
     end
 
-    private
+  private
+
+    def require_redirect?
+      env['HTTP_HOST'] =~ STARTS_WITH_WWW && host_match?
+    end
+
+    def host_match?
+      @options[:only_hosts].include?(env['HTTP_HOST']) unless @options[:only_hosts].empty?
+    end
+  
     def no_www_request(env)
       { 'Location' => Rack::Request.new(env).url.sub(/www\./i, ''),
         'Content-Type' => 'text/html' }
